c4677d76ee92c727a3a1c0c9fcf14d306f504a5a
Jimmy Li <jmyli562@gmail.com>
2023-04-16 11:54:47 -0700

    finishes lab

--- main.js
+++ main.js
@@ -4,7 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  const hide = document.getElementById("modal");
 
+  const heart = document.getElementsByClassName("like-glyph")[0];
+  heart.addEventListener("click", () => {
+    const serverCall = mimicServerCall()
+    .then(function(){
+      if(heart.className === "like-glyph" || heart.className === ""){
+        heart.textContent = FULL_HEART;
+        heart.className = "activated-heart";
+      }else if(heart.className === "activated-heart"){
+        heart.textContent = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error){
+      hide.className = "";
+      const message = document.getElementById("modal-message")
+      message.textContent = error;
+      
+      setTimeout(() => {
+        hide.className = "hidden";
+      }, "3000")
+    })
+  })
+})
 
 
 //------------------------------------------------------------------------------
